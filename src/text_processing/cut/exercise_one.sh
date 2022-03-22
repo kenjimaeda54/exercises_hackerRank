45cda006b220ccc9aed1bd46ec9491d4ee767681
ricardo_maeda_jussi <ricardo_maeda_jussi@whirlpool.com>
2022-03-21 21:33:32 -0300

    feat: finished dois exercises text procesing

---
+++ src/text_processing/cut/exercise_one.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Given N  lines of input, print 3rd  the character from each line as a new line#  of output. It is guaranteed that each of the lines of input will have a chara# cter.
+
+cat $HOME/Documents/exericios-hackerRank/src/text_processing/database/exercise_one|cut  -b3  
---
+++ src/text_processing/cut/exercise_two.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#Display the 2nd and 7th character from each line of text
+ 
+cat ~/Documents/exericios-hackerRank/src/text_processing/database/exercise_one| cut  -b 2,7  
+  

a9ca84673fd7c9f87bc5677e69fe3f1a99f69470
kenjimaeda54 <ricardo_maeda_jussi@whirlpool.com>
2022-03-21 21:52:52 -0300

    feat: finished exercise three

---
+++ src/text_processing/cut/exercise_three.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Display a range of characters starting at the 2nd  position of a string and en# ding 7th at the position (both positions included).
+
+cat ~/Documents/exericios-hackerRank/src/text_processing/database/repo_one| cut -c 2-7
+
+

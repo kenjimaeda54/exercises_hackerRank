fc03279b0176fce36c6c330487b5d4bec8280991
kenjimaeda54 <kenjimaedafamily@gmail.com>
2022-03-31 22:48:23 -0300

    feat: i relized exercise one and two with command sort

---
+++ src/text_processing/sort/first_exercise.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Given a text file, order the lines in lexicographical order.
+
+#https://stringfixer.com/pt/Colexicographical_order
+
+cat ~/Documents/exercises_hackerRank/src/text_processing/database/seven_repo.txt | sort -n
